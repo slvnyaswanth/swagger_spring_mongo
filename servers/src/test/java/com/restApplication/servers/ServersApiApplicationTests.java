6a0227e572a00171b55a9af9b910f9e17bb1bb8a
yaswanth_slvn <85479401+slvnyaswanth@users.noreply.github.com>
2023-03-29 20:27:35 +0530

    Add files via upload

---
+++ servers/src/test/java/com/restApplication/servers/ServersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restApplication.servers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServersApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
